833afde89925d2f911f326970aedaf542a96d24a
Andrew Nkole <andynkole@gmail.com>
2024-11-13 16:23:32 +0300

    ..

--- task 2.py
+++ task 2.py
@@ -39,4 +39,4 @@ for i, (combo, points) in enumerate(valid_combinations, 1):
     print(f"Комбинация {i}: " f"{combo}," f" Очки выживания: {points}")
 
 
-print(f"Всего возможных комбинаций: {len(valid_combinations)}"
+print(f"Всего возможных комбинаций: {len(valid_combinations)}")
